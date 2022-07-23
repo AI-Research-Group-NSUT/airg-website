e0e865585697535e8e30e5eb9d7473c7727133a8
Priyanshu Sharma <inbox.priyanshu@gmail.com>
2022-07-23 17:43:16 +0530

    menu styling moved to its own module

--- components/Menu/Menu.jsx
+++ components/Menu/Menu.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react'
+import * as styles from './Menu.module.css'
+
 
 export const Menu = ({ open }) => {
     return (
-        <div className={`menu_btn ${open ? 'open' : ''}`} id='btn'>
-            <div className={`menu_btn__burger`}></div>
+        <div className={`${styles.menu_btn} ${open ? styles.open : ''}`} id='btn'>
+            <div className={styles.menu_btn__burger}></div>
         </div>
     )
 }
--- sections/ScreenCoverNavbar/ScreenCoverNavbar.jsx
+++ sections/ScreenCoverNavbar/ScreenCoverNavbar.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import * as styles from './ScreenCoverNavbar.module.css'
 
 export const ScreenCoverNavbar = ({ open }) => {
-
-
-
     return (
         <div className={`${styles.cover} ${open ? styles.open : ''}`}></div>
     )
