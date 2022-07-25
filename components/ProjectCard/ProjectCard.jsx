c240a41975dadd3db40c7d8cd4a13c1a8a88a6ac
Priyanshu Sharma <inbox.priyanshu@gmail.com>
2022-07-25 13:21:00 +0530

    projetcs page

--- components/ProjectCard/ProjectCard.jsx
+++ components/ProjectCard/ProjectCard.jsx
@@ -1,23 +1,44 @@
 import Image from 'next/image'
-import React from 'react'
+import Link from 'next/link'
+import { useState } from 'react'
+import { openInNew } from '../../constants'
+
 import * as styles from './ProjectCard.module.css'
 
 export const ProjectCard = ({ project }) => {
 
+    const [showViewMore, setShowViewMore] = useState(false)
+
+    const show = () => setShowViewMore(true)
+    const hide = () => setShowViewMore(false)
+
     return (
-        <div className={styles.container}>
-            <div className={styles.imageContainer}>
-                <Image layout='responsive' className={styles.image} src={`/project-images/${project.title}.jpeg`} width={392} height={264} alt={project.title} /> {/* assuming that image name is the same as the title */}
-            </div>
-            <div className={styles.content}>
-                <div className={styles.headingSection}>
-                    <div className={styles.heading}>{project.title}</div>
-                    <div className={styles.date}>{project.date}</div>
+        <Link href={`/projects/${project.title}`}>
+
+            <div onMouseEnter={show} onMouseLeave={hide} >
+
+                <div className={styles.container}>
+                    <div className={styles.imageContainer}>
+                        <Image layout='responsive' className={styles.image} src={`/project-images/${project.title}.jpeg`} width={392} height={264} alt={project.title} /> {/* assuming that image name is the same as the title */}
+                    </div>
+                    <div className={styles.content}>
+                        <div className={styles.headingSection}>
+                            <div className={styles.heading}>{project.title}</div>
+                            <div className={styles.date}>{project.date}</div>
+                        </div>
+                        <div className={`descriptionSection ${styles.description}`}>
+                            {project.description}
+                        </div>
+
+                    </div>
+                    <div className={`${styles.viewMore} ${showViewMore ? styles.active : ''}`}>
+                        <span className={styles.openInNew}>
+                            {openInNew}
+                        </span>
+
+                    </div>
                 </div>
-                <div className='descriptionSection'>
-                    {project.description}
-                </div>
             </div>
-        </div>
+        </Link>
     )
 }
--- pages/projects.js
+++ pages/projects.js
@@ -1,4 +1,9 @@
 import Head from "next/head";
+import ProjectCard from "../components/ProjectCard";
+
+import * as styles from "../styles/Projects.module.css";
+
+
 
 const Project = () => {
   return (
@@ -6,8 +11,48 @@ const Project = () => {
       <Head>
         <title>Projects</title>
       </Head>
+      <div className={styles.container}>
+        <div className="heading-large">Projects</div>
+        <div className={`${styles.textSection} body`}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit ea in, et vero aut culpa, corporis tempora illum molestias quis harum aliquam saepe, fugiat quod quasi numquam voluptatibus. Officia est nihil porro alias quo eius in necessitatibus animi ipsa assumenda odit, ad totam delectus? Voluptatibus modi ut similique provident consequuntur a aliquid, nostrum officia, debitis alias, quas nam animi fuga excepturi id dolorem recusandae ea sapiente error architecto cupiditate optio quaerat dignissimos veritatis? Ipsam corrupti officiis doloremque laboriosam minima consectetur?
+        </div>
+
+        <div className={styles.projectCardSection}>
+          <div className={styles.subSectionHeading}>Public Health</div>
+          <div className={styles.cardSection}>
+            <ProjectCard project={{ title: 'Project-1', date: 'XXXX/XX/XX', description: 'Unraveling domestic level human mobility trends and patterns in COVID with Facebook movement Data.' }} />
+            <ProjectCard project={{ title: 'Project-2', date: 'XXXX/XX/XX', description: 'Analysing the relations of increasing Particulate Matter (PM) levels and the increase in the cases  and deaths due to respiratory diseases, in Delhi.' }} />
+          </div>
+
+
+          <div className={styles.subSectionHeading}>Computer Vision</div>
+          <div className={styles.cardSection}>
+            <ProjectCard project={{ title: 'Project-1', date: 'XXXX/XX/XX', description: 'Style transfer for images. Lorem ipsum blah vlah blah  ' }} />
+
+          </div>
+
+          <div className={styles.subSectionHeading}>Natural Language Processing</div>
+          <div className={styles.cardSection}>
+            <ProjectCard project={{ title: 'Project-1', date: 'XXXX/XX/XX', description: 'Synthesizing Devanagari Script Handwriting Using Deep Recurrent Neural Networks.' }} />
+            <ProjectCard project={{ title: 'Project-2', date: 'XXXX/XX/XX', description: 'Multilingual Toxic Comments Classification using DistilBERT and RoBERTa' }} />
+          </div>
+
+          <div className={styles.subSectionHeading}>Climate Change</div>
+          <div className={styles.cardSection}>
+            <ProjectCard project={{ title: 'Project-1', date: 'XXXX/XX/XX', description: 'Climate change impacts on increasing surface temperatures and heat wave patterns in South Asia.' }} />
+
+          </div>
+
+        </div>
+
+
+      </div>
     </div>
   );
 };
 
 export default Project;
+
+
+
+
