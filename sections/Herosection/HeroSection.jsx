5565365815c6eb967c776e223fecc67e1856751b
Priyanshu Sharma <inbox.priyanshu@gmail.com>
2022-09-16 22:11:19 +0530

    changed herosection

--- sections/Herosection/HeroSection.jsx
+++ sections/Herosection/HeroSection.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
+import { HeaderSceneThree } from '../../components/HeaderSceneThree'
 
 import * as styles from './HeroSection.module.css'
 
 export const HeroSection = () => {
     return (
         <div className={styles.heroSection}>
-            <div className={styles.heading}>Artificial Intelligence <br />
-                Research Group</div>
-            <div className={styles.subHeading}>
-                Netaji Subhas University of Technology
+            <div className={styles.three}>
+
+                <HeaderSceneThree />
+            </div>
+
+            <div className={styles.gradient}>
+
+            </div>
+
+            <div className={styles.textSection}>
+
+                <div className={styles.heading}>Artificial Intelligence <br />
+                    Research Group</div>
+                <div className={styles.subHeading}>
+                    Netaji Subhas University of Technology
+                </div>
             </div>
         </div>
     )
