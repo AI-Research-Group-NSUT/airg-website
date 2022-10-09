a64afee4404658cb739d56070dd6b43a0c5f47fe
Priyanshu Sharma <inbox.priyanshu@gmail.com>
2022-10-09 17:32:14 +0530

    --

--- components/TeamCardsHolder.jsx
+++ components/TeamCardsHolder.jsx
@@ -1,21 +1,18 @@
-import styles from "../styles/team.module.css"
+import styles from "../styles/team.module.css";
 import { TeamCard } from "./TeamCard";
 
-
 export const TeamCardsHolder = ({ heading, data, subHead }) => {
-    return (<>
-        <div className={styles.teamParent}>
-            <div className={styles.heading}>
-                {heading}
-            </div>
-            <div className={styles.subHeading}>
-                {subHead ?? ""}
-            </div>
-            <div className={styles.cardWrap}>
-                {data?.map((member, index) => {
-                    return <TeamCard member={member} key={index} />
-                })}
-            </div>
-        </div>  
-    </>);
-}
+  return (
+    <>
+      <div className={styles.teamParent}>
+        <div className={styles.heading}>{heading}</div>
+        <div className={styles.subHeading}>{subHead ?? ""}</div>
+        <div className={styles.cardWrap}>
+          {data?.map((member, index) => {
+            return <TeamCard member={member} key={index} />;
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
