af93f481ad6c3f129ccea12fbf843b89a73e94c6
Naman Dureja <ndureja@Namans-Air-2.Dlink>
2022-10-11 21:44:27 +0530

    appostophe escape

--- next.config.js
+++ next.config.js
@@ -2,6 +2,19 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        path: false,
+        child_process: false,
+        net: 'empty',
+        dns: 'empty',
+        tls: 'empty',
+      }
+    }
+    return config
+  }
 };
 
 module.exports = nextConfig;
--- pages/events.js
+++ pages/events.js
@@ -2,8 +2,6 @@ import Head from "next/head";
 import ShowcaseCard from "../components/ShowcaseCard";
 import * as styles from "../styles/Events.module.css";
 
-import { google } from "googleapis";
-
 import { Spacer } from "../components/Spacer";
 const lorem =
   "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet ipsam esse voluptatibus sed similique natus debitis maxime vero quidem obcaecati nulla, minima fuga aperiam veritatis quae, error eaque excepturi, ut labore molestiae ipsa! Nesciunt repellendus dolor odio hic deserunt, ea eligendi, id ipsam, reprehenderit minus assumenda veniam est eum neque!";
@@ -36,58 +34,58 @@ const Events = () => {
   );
 };
 
-async function addDataToGoogleSheets(data) {
-  const auth = new google.auth.GoogleAuth({
-    keyFile: process.env.GOOGLE_CREDENTIALS,
-    scopes: "https://www.googleapis.com/auth/spreadsheets",
-  });
+// async function addDataToGoogleSheets(data) {
+//   const auth = new google.auth.GoogleAuth({
+//     keyFile: process.env.GOOGLE_CREDENTIALS,
+//     scopes: "https://www.googleapis.com/auth/spreadsheets",
+//   });
 
-  const client = await google.auth.getClient({
-    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
-  });
+//   const client = await google.auth.getClient({
+//     scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+//   });
 
-  const googleSheets = google.sheets({ version: "v4", auth: client });
+//   const googleSheets = google.sheets({ version: "v4", auth: client });
 
-  const spreadsheetId = process.env.GOOGLE_SHEET_ID;
+//   const spreadsheetId = process.env.GOOGLE_SHEET_ID;
 
-  await googleSheets.spreadsheets.values.append({
-    auth,
-    spreadsheetId,
-    range: "Sheet1!A1",
-    valueInputOption: "USER_ENTERED",
-    resource: {
-      values: [[data.name, data.email, data.message]],
-    },
-  });
-}
+//   await googleSheets.spreadsheets.values.append({
+//     auth,
+//     spreadsheetId,
+//     range: "Sheet1!A1",
+//     valueInputOption: "USER_ENTERED",
+//     resource: {
+//       values: [[data.name, data.email, data.message]],
+//     },
+//   });
+// }
 
-export async function getServerSideProps(context) {
-  // const auth = await google.auth.getClient({
-  //   scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
-  // });
+// export async function getServerSideProps(context) {
+//   // const auth = await google.auth.getClient({
+//   //   scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
+//   // });
 
-  // const sheets = google.sheets({ version: "v4", auth });
+//   // const sheets = google.sheets({ version: "v4", auth });
 
-  // const range = "Sheet1!A1:G1";
+//   // const range = "Sheet1!A1:G1";
 
-  // const res = await sheets.spreadsheets.values.get({
-  //   spreadsheetId: process.env.GOOGLE_SHEET_ID,
-  //   range,
-  // });
+//   // const res = await sheets.spreadsheets.values.get({
+//   //   spreadsheetId: process.env.GOOGLE_SHEET_ID,
+//   //   range,
+//   // });
 
-  // console.log(res.data.values);
+//   // console.log(res.data.values);
 
-  const res2 = await addDataToGoogleSheets({
-    name: "test",
-    email: "test",
-    message: "test",
-  });
+//   const res2 = await addDataToGoogleSheets({
+//     name: "test",
+//     email: "test",
+//     message: "test",
+//   });
 
-  console.log;
+//   console.log;
 
-  return {
-    props: {}, // will be passed to the page component as props
-  };
-}
+//   return {
+//     props: {}, // will be passed to the page component as props
+//   };
+// }
 
 export default Events;
